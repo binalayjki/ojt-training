164297efb1adf6ab993da582edf89eb2b50903a5
binalayjki <binalayjki@gmail.com>
2026-02-02 11:47:05 +0800

    My second Commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  title = 'OJT Training Application';
+  greeting = 'Hahaha';
+
   constructor() { }
 
   ngOnInit() {
